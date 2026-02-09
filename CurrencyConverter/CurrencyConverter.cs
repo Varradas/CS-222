0da15b4f035335f32e86cb0a083076d552a633d1
Varradas <vinzdc10@gmail.com>
2026-02-09 10:31:49 +0800

    ...

---
+++ CurrencyConverter/CurrencyConverter.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter amount in SAR: ");
+double sar = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter exchange rate from SAR to PHP: ");
+double exchangeRate = Convert.ToDouble(Console.ReadLine());
+CurrencyConverter(sar, exchangeRate);
+
+void CurrencyConverter(double sar, double er){
+    double php = sar * er;
+    Console.WriteLine("Amount in PHP: {0:F2}", php);
+}
---
+++ Interest Calculator/InterestCalculator.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter Principal amount: ");
+double principal = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter rate of interest: ");
+double interest = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter time in years: ");
+double years = Convert.ToDouble(Console.ReadLine());
+InterestCalculator(principal, interest, years);
+
+void InterestCalculator(double p, double i, double y){
+    double simpleInterest = (p * i * y) / 100;
+    Console.WriteLine("Simple Interest: {0:F2}", simpleInterest);
+}
