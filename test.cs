023ebf20ff4b3ed29122b807a61a1d1209bae728
Varradas <167406865+Varradas@users.noreply.github.com>
2026-02-25 10:34:28 +0800

    Add files via upload

---
+++ test.cs
@@ -0,0 +1,14 @@
+using system;
+class test
+{
+    public static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+//dotnet new console -n MyApp
+//dotnet new webapp -n MyWebApp
+//dotnet new classlib -n MyLibrary
+//dotnet run --project ./file/path/here.csproj
+//taskkill /F /IM dotnet.exe
